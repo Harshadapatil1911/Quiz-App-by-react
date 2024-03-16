03d50bd64116dc208cb3431047fb52274d156434
“Harshada_Patil” <patilharshada1911@gmail.com>
2024-03-16 15:21:17 +0530

    adding timing function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Loading from './Components/Loading';
 import QuizForm from './Components/QuizForm';
@@ -15,11 +16,46 @@ const App = () => {
     checkAnswer
   } = useGlobalContext();
 
+  const [timer, setTimer] = useState(5); // Initial timer value in seconds
+  const [timerRunning, setTimerRunning] = useState(false);
+
+  useEffect(() => {
+    let timerInterval;
+    if (timerRunning) {
+      timerInterval = setInterval(() => {
+        setTimer((prevTimer) => {
+          // Decrease timer by 1, but make sure it doesn't go below 0
+          if (prevTimer > 0) {
+            return prevTimer - 1;
+          } else {
+            return 0;
+          }
+        });
+      }, 1000);
+    }
+    return () => clearInterval(timerInterval);
+  }, [timerRunning]);
+
+  useEffect(() => {
+    if (timer === 0) {
+      handleAnswerClick(null); // Automatically submit answer when timer runs out
+    }
+  }, [timer]);
+
+  const startTimer = () => {
+    setTimer(5); // Reset timer
+    setTimerRunning(true);
+  };
+
+  const stopTimer = () => {
+    setTimerRunning(false);
+  };
+
   if (waiting) {
     return <QuizForm />;
   }
-  if(loading){
-    return <Loading/>;
+  if (loading) {
+    return <Loading />;
   }
 
   const { question, incorrect_answers, correct_answer } = questions[index];
@@ -34,15 +70,17 @@ const App = () => {
   }
 
   const handleAnswerClick = (answer) => {
+    stopTimer(); // Stop timer when an answer is selected
     checkAnswer(answer === correct_answer);
+    nextQuestion();
   };
 
   return (
     <main>
-      <Modal/>
+      <Modal />
       <section className='quiz'>
-        <p> Correct Answer: {correct}/{index}</p>
-        <article className='container'>
+        <p>Correct Answer: {correct}/{index}</p>
+        <div>
           <h2 dangerouslySetInnerHTML={{ __html: question }} />
           <div>
             {answers.map((answer, index) => (
@@ -55,8 +93,9 @@ const App = () => {
               />
             ))}
           </div>
-        </article>
+        </div>
         <button className='btn btn-warning next-question' style={{ width: "20%", marginRight: "1rem" }} onClick={nextQuestion}>Next Question</button>
+        <div className="timer">Time Left: {timer} seconds</div>
       </section>
     </main>
   );
